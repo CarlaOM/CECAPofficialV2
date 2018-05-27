dfaedfb919659d4a714c1e5e285ffc5c86f6c131
CarlaOM <30482992+CarlaOM@users.noreply.github.com>
2018-05-27 19:19:36 -0400

    Details profile

--- fe/src/app/event/profile-person/details/details.component.ts
+++ fe/src/app/event/profile-person/details/details.component.ts
@@ -12,8 +12,13 @@ export class DetailsComponent implements OnInit {
   public date;
   public profileId;
   public personId;
+  public details;
   public program;
-  public programName;
+  public modulars: Array<any> = [];
+  public profile;
+  public requirements;
+  public finalWork;
+  public facilitator;
 
   constructor(
     private route: ActivatedRoute,
@@ -38,13 +43,49 @@ export class DetailsComponent implements OnInit {
   
     this._peticionesService.postPersonProgramDetails(this.personId, this.profileId,).subscribe(
       result => {
-        //  this.program = result;
+        this.details = result
          console.log(result)
-        //  this.programPerson = this.person.profile.programs;
-        //  console.log(this.programPerson)
+
+         this.profile = this.details.profile;
+         this.requirements = this.details.profile.requirements; //console.log(this.requirements)
+         this.finalWork = this.details.profile.final_work; console.log(this.finalWork)
+        
+         this.modules();
+         this.findProgram();
+         this.findFacilitator();
       },
       error => {
          console.log(<any>error);
       });
+  }
+  findProgram() {
+    // console.log(this.programId)
+    this._peticionesService.getProgram(this.profile.programs).subscribe(
+       result => {
+          this.program = result;
+          // console.log(this.program)
+       },
+       error => {
+          console.log(<any>error);
+       })
+  }
+  modules() {
+    for (let i = 0; i <= this.modulars.length; i++) {
+      this.modulars.pop(); i = 0;
+    }
+    for (let i of this.details.modulars) {
+      this.modulars.push(i);
+    }
+  }
+  findFacilitator() {
+    // console.log(this.programId)
+    this._peticionesService.getFacilitador(this.finalWork.facilitator).subscribe(
+       result => {
+          this.facilitator = result;
+          console.log(this.facilitator)
+       },
+       error => {
+          console.log(<any>error);
+       })
   }
 }
