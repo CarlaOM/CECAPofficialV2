a8aaa22066730c77642a534e71ca3c16d2ad1a2d
alvarodaniel294 <alvarodaniel294@gmail.com>
2018-05-30 13:07:37 -0400

    vista PRincipal

--- be/routes/cajaPrincipal.js
+++ be/routes/cajaPrincipal.js
@@ -11,30 +11,37 @@ router
         console.log(req.body);
         let cashOffice=req.body;
         let detail={};
-        detail.cashFlowOffices=cashOffice._id;
-        detail.dateCloseCash=new Date();
-        detail.amount=cashOffice.amount_delivered;
-        detail.description='hola';
-        detail.input=true;
-        detail.title='este es el titulo';
-        console.log('asdfasdfasdfasdfasfasdf')
-        console.log(detail);
-        console.log('1111111111111111111111')
-        console.log(cashOffice.offices);
-        console.log('2222222222222222222222222')
-        console.log('officesid'+req.body.offices)
-        db.cashFlowPrincipal.findOne({offices:req.body.offices},function(err,cashPrincipal){
-         
-            if(err)res.status(400).send(err);
-            console.log('eseta es la caja principal');
-            console.log(cashPrincipal);
-            cashPrincipal.amount+=detail.amount;
-            cashPrincipal.details.push(detail);
-            cashPrincipal.save(function(err,cashP){
-                if(err)res.status(400).send(err);
-                return res.status(200).send(cashP);
+        db.cashFlowOffices.findOne({_id:cashOffice._id},function(err,cashFOffice){
+
+            db.offices.findOne({_id:cashFOffice.offices},function(err,off){
+                detail.officeName=off.name;
+                console.log(off);
+                detail.cashFlowOffices=cashOffice._id;
+                detail.dateCloseCash=new Date();
+                detail.amount=cashOffice.amount_delivered;
+                detail.description='';
+                detail.input=true;
+                detail.title='Cierre de Caja';
+                // console.log('asdfasdfasdfasdfasfasdf')
+                // console.log(detail);
+                // console.log('1111111111111111111111')
+                // console.log(cashOffice.offices);
+                // console.log('2222222222222222222222222')
+                // console.log('officesid'+req.body.offices)
+                db.cashFlowPrincipal.findOne({offices:req.body.offices},function(err,cashPrincipal){
+                 
+                    if(err)res.status(400).send(err);
+                    // console.log('eseta es la caja principal');
+                    // console.log(cashPrincipal);
+                    cashPrincipal.amount+=detail.amount;
+                    cashPrincipal.details.push(detail);
+                    cashPrincipal.save(function(err,cashP){
+                        if(err)res.status(400).send(err);
+                        return res.status(200).send(cashP);
+                    })
+                })
             })
-        })
+        })    
         
 
    })
